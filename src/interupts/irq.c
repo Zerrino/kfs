23938315e9e45bfabe08d14aac41aa18466bfa33
root <root@Zerrino.localdomain>
2025-08-02 16:00:58 +0200

    desactiver irq timer le temps de trouver pourquoi ca crash aahhaha

--- src/interupts/irq.c
+++ src/interupts/irq.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   irq.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rperez-t <rperez-t@student.s19.be>         +#+  +:+       +#+        */
+/*   By: alexafer <alexafer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 20:34:40 by zerrino           #+#    #+#             */
-/*   Updated: 2025/07/09 12:22:48 by rperez-t         ###   ########.fr       */
+/*   Updated: 2025/08/02 16:00:22 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	IRQ_Handler(t_registers* regs)
 		printnbr(irq, 10);
 		terminal_writestring("\n");
 	}
-	
+
 	PIC_SendEOF(irq);
 }
 
@@ -36,13 +36,13 @@ void	IRQ_Initialize()
 	PIC_Configure(PIC1_OFFSET, PIC2_OFFSET);
 	for (int j = 0; j < 16; j++)
 		PIC_Mask(j);
-	
+
 	for (size_t i = 0; i < 16; i++)
 		ISR_RegisterHandler(PIC1_OFFSET + i, IRQ_Handler);
 
 	IRQ_RegisterHandler(0, timer);
 	IRQ_RegisterHandler(1, keyboard_handler);
-	PIC_Unmask(0); // Timer
+	//PIC_Unmask(0); // Timer
 	PIC_Unmask(1); // Keyboard
 }
 
