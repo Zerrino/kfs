cb20d682b0bd3d5df7e7cc46ca28d95ba7fbe33b
Alexandre Ferreira Da Silva <alexafer@shi-c2-p3.s19.be>
2025-10-09 15:47:52 +0200

    bon va falloir refaire malloc

--- src/shell/multiboot/memory.c
+++ src/shell/multiboot/memory.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 14:13:48 by zerrino           #+#    #+#             */
-/*   Updated: 2025/10/09 14:11:27 by alexafer         ###   ########.fr       */
+/*   Updated: 2025/10/09 15:47:38 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@ uint32_t    *page_directory = (uint32_t *)0x00001000;
 uint32_t    *page_directory_info = (uint32_t *)0x00002000;
 uint32_t    *page_tables = (uint32_t *)0x00400000;
 
-void    *getPage(void *vm_addr)
+void    *get_physaddr(void *virtualaddr)
 {
-    uint32_t    pd_index = ((uint32_t)vm_addr >> 22);
-    uint32_t    pt_index = ((uint32_t)vm_addr >> 12) & ((1<<10) - 1);
-    terminal_writestring("\npd_index : 0x");
-    printnbr(pd_index, 16);
-    terminal_writestring("\npt_index : 0x");
-    printnbr(pt_index, 16);
-    terminal_writestring("\nvalue of the table index : 0x");
-    printnbr(((uint32_t *)(page_directory[pd_index] & ~((1 << 12)-1)))[pt_index] , 16);
-    terminal_writestring("\n");
-    return 0;
+    uint32_t    pd_index = ((uint32_t)virtualaddr >> 22);
+    uint32_t    pt_index = ((uint32_t)virtualaddr >> 12) & ((1<<10)-1);
+    return (void *)(((uint32_t *)(page_directory[pd_index] & ~((1 << 12)-1)))[pt_index] & ~((1 << 12)-1));
+}
+
+void    map_page(void *physaddr, void *virtualaddr, unsigned int flags)
+{
+    uint32_t    pd_index = ((uint32_t)virtualaddr >> 22);
+    uint32_t    pt_index = ((uint32_t)virtualaddr >> 12) & ((1<<10) - 1);
+
+    ((uint32_t *)(page_directory[pd_index] & ~((1 << 12)-1)))[pt_index] = (uint32_t)physaddr | (flags & ((1 << 12)-1)) | 1;
 }
 
 void    initMemory()
@@ -38,9 +39,7 @@ void    initMemory()
 
     while (i < 1024)
     {
-        page_directory[i] = 0;
-        if (i < 256)
-            page_directory[i] = (uint32_t)(&page_tables[i << 10]) | PD_FLAG_PRESENT | PD_FLAG_R_AND_W;
+        page_directory[i] = (uint32_t)(&page_tables[i << 10]) | PD_FLAG_PRESENT | PD_FLAG_R_AND_W;
         i++;
     }
     i = 0;
@@ -49,7 +48,6 @@ void    initMemory()
         page_directory_info[i] = 0;
         i++;
     }
-
     i = 0;
     while (i < (1024 << 10))
     {
@@ -58,7 +56,14 @@ void    initMemory()
             page_tables[i] = (i << 12) | PT_FLAG_PRESENT | PT_FLAG_R_AND_W;
         i++;
     }
-   activate_paging(page_directory);
+    activate_paging(page_directory);
 
-   getPage((void *)0xC01000);
+    terminal_writestring("\nvalue of the table index : 0x");
+    printnbr((uint32_t)get_physaddr((void *)0xc00000), 16);
+    terminal_writestring("\n");
+
+    char x[7] __attribute__((aligned(0x1000))) = "\nTest\n";
+    char *test = (char *)0xb0000000;
+    map_page((void *)(&x), (void*)0xb0000000, PT_FLAG_PRESENT | PT_FLAG_R_AND_W);
+    terminal_writestring(test);
 }
